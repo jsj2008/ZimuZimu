ccc73ded2bd55b050d24e2eed0f2e7e966c4540f
BflyT <Redpower@Redpower.local>
2017-03-01 16:32:56 +0800

    error

--- Pods/Target Support Files/Pods-Zimu/Pods-Zimu-frameworks.sh
+++ Pods/Target Support Files/Pods-Zimu/Pods-Zimu-frameworks.sh
@@ -96,11 +96,8 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/PYSearch/PYSearch.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
-<<<<<<< HEAD
   install_framework "$BUILT_PRODUCTS_DIR/YMCitySelect/YMCitySelect.framework"
-=======
   install_framework "$BUILT_PRODUCTS_DIR/YYModel/YYModel.framework"
->>>>>>> origin/master
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
@@ -110,11 +107,8 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/PYSearch/PYSearch.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
-<<<<<<< HEAD
   install_framework "$BUILT_PRODUCTS_DIR/YMCitySelect/YMCitySelect.framework"
-=======
   install_framework "$BUILT_PRODUCTS_DIR/YYModel/YYModel.framework"
->>>>>>> origin/master
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
